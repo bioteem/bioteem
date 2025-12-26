8d7d0a640300cdfe28d590d961e634fc343a759a
Sidak-Singh <sidaksinghkalra82@gmail.com>
2025-12-26 13:46:24 +0530

    ph

--- backend/src/api/admin/orders/[id]/freightcom/rates/route.ts
+++ backend/src/api/admin/orders/[id]/freightcom/rates/route.ts
@@ -244,6 +244,9 @@ export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
         region: "NS",
         country: "CA",
         postal_code: "B0N2T0",
+        phone_number:{
+          number:"9023064110"
+        }
       },
       residential: true,
       email_addresses: [process.env.WAREHOUSE_EMAIL || "sales@bioteem40.ca"],
